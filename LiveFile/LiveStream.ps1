ec8718d6d3f2bad0d050d65c265ab84d9864d7c1
Kevin Bost <kitokeboo@gmail.com>
2019-12-12 20:35:59 -0800

    Updating output message

--- LiveFile/LiveStream.ps1
+++ LiveFile/LiveStream.ps1
@@ -1,1 +1,1 @@
-dotnet run --countdown-to 21:00 --end-message "soon" --timer-format "\S\t\r\e\a\m\ \S\t\a\r\t\i\n\g\ \i\n\ mm\:ss"
+dotnet run --countdown-to 21:00 --end-message "Stream Starting Soon" --timer-format "\S\t\r\e\a\m\ \S\t\a\r\t\i\n\g\ \i\n\ mm\:ss"
