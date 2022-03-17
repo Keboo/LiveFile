5f0a517b42e3d2d598ef6337d6657f5756f5babd
Kevin Bost <kitokeboo@gmail.com>
2022-03-16 21:53:30 -0700

    Core sample in place.
    Details update
    
    
    More simplification
    
    
    Adding app description
    
    
    Updating to DragonFruit
    
    
    Updating NRT

--- LiveFile/Program.cs
+++ LiveFile/Program.cs
@@ -1,94 +1,50 @@
-﻿using System;
 using System.CommandLine;
 using System.CommandLine.IO;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace LiveFile;
-
-class Program
+public class Program
 {
     /// <summary>
-    /// Write updates to a file
+    /// A countdown timer application
     /// </summary>
-    /// <param name="console"></param>
     /// <param name="countdownTo">The target time to count down to</param>
-    /// <param name="endMessage">The message to display when the count down is not running.</param>
-    /// <param name="timerFormat">The format for the time display. Any valid C# format string. </param>
-    /// <param name="outputFile">The file to write to.</param>
-    /// <param name="keepFile">Delete file when program terminates.</param>
+    /// <param name="outputFile">The output file to write to</param>
     /// <param name="verbosity">The verbosity of the output</param>
-    static async Task Main(
-        IConsole console,
-        DateTime countdownTo,
-        string? endMessage = null,
-        string timerFormat = @"{0:mm\:ss}",
-        string outputFile = "output.txt",
-        bool keepFile = false,
-        Verbosity verbosity = Verbosity.Normal)
+    /// <param name="console">The console</param>
+    /// <returns></returns>
+    public static async Task Main(DateTime countdownTo, FileInfo? outputFile, Verbosity verbosity, IConsole console)
     {
+        if (outputFile is null)
+        {
+            outputFile = new FileInfo("output.txt");
+        }
         if (verbosity >= Verbosity.Normal)
         {
-            console.Out.WriteLine($"Running countdown to {countdownTo} in '{Path.GetFullPath(outputFile)}'");
+            //NB: WriteLine is an extenion method
+            console.Out.WriteLine($"Running countdown to {countdownTo} in '{outputFile!.FullName}'");
         }
 
         while (countdownTo > DateTime.Now)
         {
-            string newText;
+            string newText = string.Format(@"{0:mm\:ss}", (countdownTo - DateTime.Now));
+            if (verbosity >= Verbosity.Detailed)
+            {
+                console.Out.WriteLine(newText);
+            }
             try
             {
-
-                newText = string.Format(timerFormat, (countdownTo - DateTime.Now));
+                await File.WriteAllTextAsync(outputFile.FullName, newText);
             }
-            catch (FormatException)
-            {
-                console.Out.WriteLine($"'{timerFormat}' is not a valid format specifier. See: https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-timespan-format-strings");
-                return;
-            }
-
-            await WriteFileAsync(console, newText, outputFile, verbosity);
+            catch (IOException)
+            { }
             await Task.Delay(250);
         }
 
-        await WriteFileAsync(console, $"{endMessage}", outputFile, verbosity);
+        await File.WriteAllTextAsync(outputFile.FullName, "Starting soon");
 
         if (verbosity > Verbosity.Normal)
         {
             console.Out.Write("Countdown complete.");
-        }
-
-        if (!keepFile)
-        {
-            if (verbosity > Verbosity.Normal)
-            {
-                console.Out.WriteLine("Press any key to close");
-                Console.ReadKey();
-            }
-            try
-            {
-                File.Delete(outputFile);
-            }
-            catch (IOException)
-            { }
-        }
-    }
-
-    private static string? _lastFileContents;
-    private static async Task WriteFileAsync(IConsole console, string contents, string file, Verbosity verbosity)
-    {
-        if (contents != _lastFileContents)
-        {
-            try
-            {
-                if (verbosity >= Verbosity.Detailed)
-                {
-                    console.Out.WriteLine(contents);
-                }
-                await File.WriteAllTextAsync(file, contents);
-                _lastFileContents = contents;
-            }
-            catch (IOException)
-            { }
         }
     }
 }
